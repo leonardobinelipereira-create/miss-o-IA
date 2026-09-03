1912f44c7c04d04325e4e0e48b8cb234a9aa8a5b
leonardobinelipereira-create <leonardo.bineli.pereira@escola.pr.gov.br>
2026-09-03 11:08:40 -0300

    gnd

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.queryselector(".caixa-principal");
+const caixaPerguntas = document.queryselector(".caixa-perguntas");
+const caixaAlternativas = document.queryselector(".caixa-alternativas");
+const caixaReultado = document.queryselector(".caixa-resultado");
+const caixaReultado = document.queryselector(".caixa-resultado");
+
+const pergunta1 = [
+    {
+    enunciado: "pergunta 1",
+    alternativas:[
+        "alternativa 1",
+         "A;ternativa 2"
+        ]
+    }
+   
+    ]
