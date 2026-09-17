222bd7279c971595d4afa6114631930b3334a5c1
leonardobinelipereira-create <leonardo.bineli.pereira@escola.pr.gov.br>
2026-09-17 08:45:47 -0300

    gag

--- script.js
+++ script.js
@@ -131,3 +131,19 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 // código omitido
+const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
+            }
+        ]
+    },
+    // código omitido…
+];
